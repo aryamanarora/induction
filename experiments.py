c8539cf0a30fd8798df4c0e52bc07761bd49cfab
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-01-10 21:50:06 +0000

    clean up experiments

--- experiments.py
+++ experiments.py
@@ -14,9 +14,25 @@ def make_corr(children: list[rc.IterativeMatcher] = [], options: Optional[dict[s
     return (corr, options)
 
 
+def make_corr_i(args: list[rc.IterativeMatcher]):
+    """Make corr but split heads individually"""
+    return make_corr(args, options={"split_heads": "b0-all"})
+
+
+def m(head: int):
+    return rc.IterativeMatcher(f"b0.a.head{head}")
+
+
 # EXPERIMENTS
 def make_experiments() -> dict[str, tuple[Correspondence, dict[str, Optional[str]]]]:
     res = {}
+
+    # shortcut matchers for useful parts of the graph
+    embeds = rc.restrict("idxed_embeds", term_early_at="b0.a")
+    a1_head = rc.IterativeMatcher("a1.ind").chain(rc.restrict("a.head.on_inp", term_if_matches=True))
+    v_matcher = a1_head.children_matcher({3})
+    q_matcher = a1_head.children_matcher({1})
+    k_matcher = a1_head.children_matcher({2})
 
     # UNSCRUBBED
     res["unscrubbed"] = make_corr()
@@ -26,9 +42,6 @@ def make_experiments() -> dict[str, tuple[Correspondence, dict[str, Optional[str
     res["not-baseline"] = make_corr([rc.IterativeMatcher("a1.not_ind")])
 
     # EMBEDDING-VALUE
-    v_matcher = (
-        rc.IterativeMatcher("a1.ind").chain(rc.restrict("a.head.on_inp", term_if_matches=True)).children_matcher({3})
-    )
     ev = v_matcher.chain("b0.a")
 
     res["v"] = make_corr([v_matcher])
@@ -36,85 +49,74 @@ def make_experiments() -> dict[str, tuple[Correspondence, dict[str, Optional[str
     res["not-ev"] = make_corr([v_matcher.chain(rc.restrict("idxed_embeds", end_depth=3))])
 
     # EMBEDDING-QUERY
-    q_matcher = (
-        rc.IterativeMatcher("a1.ind").chain(rc.restrict("a.head.on_inp", term_if_matches=True)).children_matcher({1})
-    )
     eq = q_matcher.chain("b0.a")
-
     res["q"] = make_corr([q_matcher])
     res["eq"] = make_corr([eq])
     res["not-eq"] = make_corr([q_matcher.chain(rc.restrict("idxed_embeds", end_depth=3))])
 
     # EMBEDDING-KEY
-    k_matcher = (
-        rc.IterativeMatcher("a1.ind").chain(rc.restrict("a.head.on_inp", term_if_matches=True)).children_matcher({2})
-    )
-
     res["k"] = make_corr([k_matcher])
     res["ek"] = make_corr([k_matcher.chain("b0.a")])
     res["not-ek"] = make_corr([k_matcher.chain(rc.restrict("idxed_embeds", end_depth=3))])
 
     # PREVIOUS-TOKEN-HEAD KEY
-    pth_k = k_matcher.chain(rc.restrict("idxed_embeds", term_early_at="b0.a") | rc.Regex(r"\.*not_prev\.*"))
+    pth_k = k_matcher.chain(embeds | rc.Regex(r"\.*not_prev\.*"))
     res["pth-k"] = make_corr([pth_k])
-    res["not-pth-k"] = make_corr(
-        [k_matcher.chain(rc.restrict("idxed_embeds", term_early_at="b0.a") | rc.Regex(r"\.*yes_prev\.*"))]
-    )
+    res["not-pth-k"] = make_corr([k_matcher.chain(embeds | rc.Regex(r"\.*yes_prev\.*"))])
     res["pth-k-fine"] = make_corr([k_matcher.chain(rc.Regex(r"\.*not_prev\.*"))])
-    res["not-pth-k-emb"] = make_corr(
-        [k_matcher.chain(rc.restrict("idxed_embeds", term_early_at="b0.a") | rc.Regex(r"\.*yes_prev\.*"))]
+    res["not-pth-k-emb"] = make_corr([k_matcher.chain(embeds | rc.Regex(r"\.*yes_prev\.*"))])
+
+    # include 0.6 also
+    res["not-pth-k-emb-06"] = make_corr(
+        [k_matcher.chain(embeds | m(0) | m(6))],
+        options={"split_heads": "b0-all"},
+    )
+    res["not-pth-k-06"] = make_corr(
+        [k_matcher.chain(m(0) | m(6))],
+        options={"split_heads": "b0-all"},
+    )
+    res["not-pth-k-6"] = make_corr(
+        [k_matcher.chain(m(6))],
+        options={"split_heads": "b0-all"},
+    )
+    res["not-pth-k-7"] = make_corr(
+        [k_matcher.chain(m(7))],
+        options={"split_heads": "b0-all"},
+    )
+    res["not-pth-unimp"] = make_corr(
+        [k_matcher.chain(m(1) | m(2) | m(3) | m(4) | m(5) | m(7))],
+        options={"split_heads": "b0-all"},
     )
 
     # PTH-QUERY
-    res["pth-q"] = make_corr(
-        [q_matcher.chain(rc.restrict("idxed_embeds", term_early_at="b0.a") | rc.Regex(r"\.*not_prev\.*"))]
-    )
+    res["pth-q"] = make_corr([q_matcher.chain(embeds | rc.Regex(r"\.*not_prev\.*"))])
 
     # ALL (3 og scrubbing)
     res["all"] = make_corr([ev, eq, pth_k])
 
     # a0.7
-    res["a0.7"] = make_corr([rc.IterativeMatcher("b0.a.head7")], options={"split_heads": "b0-all"})
-    res["a0.0"] = make_corr([rc.IterativeMatcher("b0.a.head0")], options={"split_heads": "b0-all"})
+    res["a0.7"] = make_corr_i([m(7)])
+    res["a0.0"] = make_corr_i([m(0)])
+
+    # scrub each head individually
+    for i in range(8):
+        res[f"a0-v-{i}"] = make_corr_i([v_matcher.chain(embeds | m(i))])
+
+    # scrub subsets of heads
+    res[f"a0-v-0,6"] = make_corr_i([v_matcher.chain(embeds | m(0) | m(6))])
+    res[f"a0-v-only0,6"] = make_corr_i([v_matcher.chain(m(0) | m(6))])
+    res[f"a0-v-only016"] = make_corr_i([v_matcher.chain(m(0) | m(1) | m(6))])
+    res[f"a0-v-not0,6"] = make_corr_i([v_matcher.chain(embeds | m(1) | m(2) | m(3) | m(4) | m(5) | m(7))])
+    res[f"a0-v-onlynot0,6"] = make_corr_i([v_matcher.chain(m(1) | m(2) | m(3) | m(4) | m(5) | m(7))])
+    res[f"a0-v-all-bad-but-2"] = make_corr_i([v_matcher.chain(embeds | m(1) | m(3) | m(4) | m(5) | m(7))])
+    res[f"a0-v-all-bad-but-12"] = make_corr_i([v_matcher.chain(embeds | m(3) | m(4) | m(5) | m(7))])
 
     return res
 
 
-def a0_heads_to_v() -> dict[str, tuple[Correspondence, dict[str, Optional[str]]]]:
-    """Check performance when scrubbing each head in layer 0 as input to a1_ind.k"""
-
-    def make_corr_v(args: list[rc.IterativeMatcher]):
-        """Make corr but split heads individually"""
-        return make_corr(args, options={"split_heads": "b0-all"})
-
-    def m(head: int):
-        return rc.IterativeMatcher(f"b0.a.head{head}")
-
-    exps = {}
-    v_matcher = (
-        rc.IterativeMatcher("a1.ind").chain(rc.restrict("a.head.on_inp", term_if_matches=True)).children_matcher({3})
-    )
-    embeds = rc.restrict("idxed_embeds", term_early_at="b0.a")
-
-    # scrub each head individually
-    for i in range(8):
-        exps[f"a0-v-{i}"] = make_corr_v([v_matcher.chain(embeds | rc.IterativeMatcher(f"b0.a.head{i}"))])
-
-    # scrub subsets of heads
-    exps[f"a0-v-0,6"] = make_corr_v([v_matcher.chain(embeds | m(0) | m(6))])
-    exps[f"a0-v-only0,6"] = make_corr_v([v_matcher.chain(m(0) | m(6))])
-    exps[f"a0-v-only016"] = make_corr_v([v_matcher.chain(m(0) | m(1) | m(6))])
-    exps[f"a0-v-not0,6"] = make_corr_v([v_matcher.chain(embeds | m(1) | m(2) | m(3) | m(4) | m(5) | m(7))])
-    exps[f"a0-v-onlynot0,6"] = make_corr_v([v_matcher.chain(m(1) | m(2) | m(3) | m(4) | m(5) | m(7))])
-    exps[f"a0-v-all-bad-but-2"] = make_corr_v([v_matcher.chain(embeds | m(1) | m(3) | m(4) | m(5) | m(7))])
-    exps[f"a0-v-all-bad-but-12"] = make_corr_v([v_matcher.chain(embeds | m(3) | m(4) | m(5) | m(7))])
-
-    return exps
-
-
 def main():
     experiments = make_experiments()
-    experiments.update(a0_heads_to_v())
+    experiments["transpose-ind"] = experiments["unscrubbed"]
 
     # parse arguments
     parser = argparse.ArgumentParser(
@@ -132,6 +134,14 @@ def main():
     loss, good_induction_candidate, tokenizer, toks_int_values = construct_circuit()
     options = experiments[args.exp_name][1] or {}
     with_a1_ind_inputs = clean_model(loss, **options)
+
+    if args.exp_name in ["transpose-ind"]:
+        with_a1_ind_inputs = with_a1_ind_inputs.update(
+            rc.IterativeMatcher("a1.ind")
+            .chain(rc.restrict("a.head.on_inp", term_if_matches=True))
+            .chain(rc.restrict("a.attn_scores_raw", end_depth=6)),
+            lambda circ: rc.Einsum.from_einsum_string("rqk -> rkq", circ),
+        )
 
     run_experiment(
         experiments,
