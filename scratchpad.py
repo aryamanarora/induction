a367457010d39a3af3b0001a05abddf5bb38f575
a_remix_user <maxnadeau@rdwrs.com>
2023-01-10 17:48:37 +0000

    Add more funcs to scratchpad

--- scratchpad.py
+++ scratchpad.py
@@ -1,51 +1,70 @@
-# %%
 import torch
+import pickle
+import plotly.express as px
+from colorama import Fore, Back, Style
 
-good_induction_candidates = torch.tensor([0, 1, 0, 1, 1])
+def load_res(res_path):
+    with open(f"data/{res_path}.pkl", "rb") as f:
+        return pickle.load(f)
 
-a = torch.tensor([[0, 1, 2, 3, 4], [0, 1, 2, 3, 1], [0, 1, 2, 1, 1], [0, 0, 0, 0, 0], [0, 1, 2, 2, 1]])
+def load_all(res_path):
+    r, m1, m2 = load_res(res_path)
+    with open(f"data/inps_{res_path}.pkl", 'rb') as f:
+        i = pickle.load(f)
+    return r, m1, m2, i
 
-
-def get_induction_candidate_mask(
-    t: torch.Tensor, good_induction_candidates: torch.Tensor, match_all_occurrences=False
-) -> torch.Tensor:
-    """
-    t is a 2d Tensor of token indices of size batch_size x seq_len
-    good_induction_candidate is a 1d Tensor of 0s and 1s of size vocab_size indicating whether the ith token is a good induction candidate in general
-    Return a 2d Tensor of bools indicating whether each token in t is a repeated occurrence of a good induction candidate in that row (or, if match_all_occurrences is True, we also set the first occurrence of the token to True)
-    """
-    res = torch.ones_like(t, dtype=torch.bool)
-    good_induction_candidates = good_induction_candidates.to(dtype=torch.bool)
-    # Sorry, couldn't find anything better than a double-for
-    for i, row in enumerate(t):
-        seen_toks = set()
-        for j, tok in enumerate(row):
-            if tok.item() in seen_toks or match_all_occurrences:
-                res[i, j] = good_induction_candidates[tok]
-            else:
-                seen_toks.add(tok.item())
-                res[i, j] = False
-
-    return res
+def get_diff_ixes(inp1, inp2):
+    diff_ixes = []
+    for i in range(inp1.shape[0]):
+        if not (inp1[i] == inp2[i]).all():
+            diff_ixes.append(i)
+    return diff_ixes
 
 
-def decode_and_highlight(toks_to_decode, highlight_ixes, tokenizer):
-    if len(toks_to_decode.shape) == 1:
-        toks_to_decode = toks_to_decode.unsqueeze(0)
+def decode_and_highlight(seq_to_decode, highlight_mask, tokenizer):
     res = []
-    for row in toks_to_decode:
-        row_toks = []
-        for i, ch in enumerate(row):
-            if highlight_ixes[ch]:
-                row_toks += [58, 58, 58]
-                row_toks.append(ch)
-                row_toks += [60, 60, 60]
-            else:
-                row_toks.append(ch)
-        res.append("".join(tokenizer.batch_decode(row_toks)))
+    for i, ch in enumerate(seq_to_decode):
+        if i == 300:
+            res.append(ch)
+            break
+        if highlight_mask[i]:
+            res += [58, 58, 58]
+            res.append(ch)
+            res += [60, 60, 60]
+        else:
+            res.append(ch)
+    res = "".join(tokenizer.batch_decode(res))
+    res = res.replace("]]][[[", f"{Style.RESET_ALL}{Fore.RED}|{Style.RESET_ALL}{Back.RED}")
+    res = res.replace("[[[", Back.RED)
+    res = res.replace("]]]", f"{Style.RESET_ALL}")
     return res
 
+def compare_losses(exp1, exp2, tokenizer, inps):
+    res_1, ind_candidates_mask_1, ind_candidates_later_occur_mask_1 = load_res(exp1)
+    res_2, ind_candidates_mask_2, ind_candidates_later_occur_mask_2 = load_res(exp2)
+    exp1_better = res_1 < res_2
+    for i in range(res_1.shape[0]):
+        print(decode_and_highlight(inps[i], ind_candidates_later_occur_mask_1[i], tokenizer))
+        print(decode_and_highlight(inps[i], exp1_better[i], tokenizer))
+        inp = input()
+        if inp == 'q':
+            break
+        else:
+            pass
 
-# %%
-get_induction_candidate_mask(a, good_induction_candidates, match_all_occurrences=True)
-# %%
+def build_hist(exp1, exp2):
+    res_1, ind_candidates_mask_1, ind_candidates_later_occur_mask_1 = load_res(exp1)
+    res_2, ind_candidates_mask_2, ind_candidates_later_occur_mask_2 = load_res(exp2)
+    data = res_1[ind_candidates_later_occur_mask_1] - res_2[ind_candidates_later_occur_mask_1]
+    fig = px.histogram(torch.flatten(data.cpu()), range_y=[0, 3000], 
+        range_x=[-10, 3])
+    fig.write_image("other_img.jpg")
+
+with open("data/tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+with open("data/inps.pkl", "rb") as f:
+    inps = pickle.load(f)
+
+    
+build_hist("unscrubbed", "baseline")
+compare_losses("a0-v-0", "not-ev", tokenizer, inps)
