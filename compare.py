6b1e4d683c2b2266a5921a9b224eab82f4214b40
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-01-12 21:47:43 +0000

    more metrics for subgraph ablation attribution

--- compare.py
+++ compare.py
@@ -1,6 +1,75 @@
-from experiments import *
 from interp import cui
 from matplotlib import pyplot as plt
+import argparse
+import torch
+from tqdm import tqdm
+
+from experiments import make_experiments, make_make_corr, run, FixedSampler
+
+
+def compare(exp1, exp2, num_samples, idx, verbose, plot=True):
+    experiments = make_experiments(make_make_corr(FixedSampler(idx)))
+
+    assert exp1 in list(experiments.keys())
+    assert exp2 in list(experiments.keys())
+
+    res1, _, _, _, inps1, tokenizer = run(
+        experiments, exp1, num_samples if exp1 != "unscrubbed" else idx + 1, False, verbose
+    )
+    res2, _, _, _, inps2, _ = run(experiments, exp2, num_samples if exp2 != "unscrubbed" else idx + 1, False, verbose)
+
+    mean1 = res1.mean(dim=0)
+    mean2 = res2.mean(dim=0)
+    res = (mean1 - mean2).cpu()
+    labs = [x.replace("\n", "\\n").replace(" ", "_") for x in tokenizer.batch_decode(inps1[0].reshape(-1))]
+
+    if plot:
+        fig, ax = plt.subplots(15, 1)
+        m = res.abs().max().item()
+        print(m)
+        res = res.reshape(15, 1, -1)
+        for i in range(15):
+            ax[i].imshow(res[i], vmin=-m, vmax=m, cmap="RdBu")
+            ax[i].set_xticks(list(range(res.shape[2])))
+            ax[i].set_yticks([])
+            ax[i].set_xticklabels(labs[i * 20 + 1 : i * 20 + 21], rotation=45)
+        plt.gcf().set_size_inches(10, 25)
+        plt.savefig(f"figs/{exp1}_{exp2}_{idx}.png")
+
+    return res, res1, res2, inps1, inps2, tokenizer
+
+
+def plot_convergence(exp1, exp2, idx, verbose):
+    """Plot convergence of mean loss differences per token vs. scrubbing sample size."""
+    ress = torch.zeros(1, 300)
+    for i in tqdm(range(1, 500, 25)):
+        res, _, _, _, _, _ = compare(exp1, exp2, i, idx, verbose, False)
+        res = res.reshape(1, -1)
+        ress = torch.cat((ress, res), 0)
+
+    for i in range(300):
+        plt.plot(list(range(1, 1000, 50)), ress[1:, i])
+    plt.xlabel("Samples")
+    plt.ylabel("Loss difference")
+    plt.title(f"{exp1} vs. {exp2} on {idx}")
+    plt.savefig(f"figs/convergence.png")
+
+
+def plot_distribution(exp1, exp2, idx, verbose):
+    """Plot distribution of loss differences between fixed inputed and varying scrubbed inputs."""
+    res, res1, res2, inps1, inps2, tokenizer = compare(exp1, exp2, 10000, idx, verbose, False)
+    l = (res1.mean(dim=0) - res2).mean(dim=1).cpu()
+    _, indices = torch.sort(l)
+    for i in indices[:1]:
+        print(i, l[i])
+        # print(tokenizer.batch_decode(inps1[i].reshape(-1)))
+        print(tokenizer.batch_decode(inps2[i].reshape(-1)))
+    # print(l)
+    plt.hist(l.tolist(), bins=100)
+    plt.ylabel("Samples")
+    plt.xlabel("Loss difference")
+    plt.title(f"{exp1} vs. {exp2} on {idx}")
+    plt.savefig(f"figs/distribution.png")
 
 
 def main():
@@ -11,6 +80,7 @@ def main():
     )
     parser.add_argument("--exp1", action="store", dest="exp_name", type=str, default="unscrubbed")
     parser.add_argument("--exp2", action="store", dest="exp2_name", type=str, default="baseline")
+    parser.add_argument("--type", action="store", dest="type", type=str, default="compare")
     parser.add_argument("--samples", action="store", dest="samples", type=int, default=10000)
     parser.add_argument("--idx", action="store", dest="idx", type=int, default=0)
     parser.add_argument("--verbose", action="store", dest="verbose", type=int, default=0)
@@ -18,37 +88,12 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    idx = args.idx
-    experiments = make_experiments(make_make_corr(FixedSampler(idx)))
-
-    assert args.exp_name in list(experiments.keys())
-    assert args.exp2_name in list(experiments.keys())
-
-    res1, _, _, _, inps1, tokenizer = run(
-        experiments, args.exp_name, args.samples if args.exp_name != "unscrubbed" else idx + 1, False, args.verbose
-    )
-    res2, _, _, _, _, _ = run(
-        experiments, args.exp2_name, args.samples if args.exp2_name != "unscrubbed" else idx + 1, False, args.verbose
-    )
-
-    mean1 = res1.mean(dim=0)
-    mean2 = res2.mean(dim=0)
-    res = (mean1 - mean2).cpu().reshape(15, 1, -1)
-    labs = [x.replace("\n", "\\n").replace(" ", "_") for x in tokenizer.batch_decode(inps1[0].reshape(-1))]
-    print(res)
-
-    fig, ax = plt.subplots(15, 1)
-    m = res.abs().max().item()
-    print(m)
-    for i in range(15):
-        ax[i].imshow(res[i], vmin=-m, vmax=m, cmap="RdBu")
-        ax[i].set_xticks(list(range(res.shape[2])))
-        ax[i].set_yticks([])
-        print(labs[i * 20 : i * 20 + 21])
-        ax[i].set_xticklabels(labs[i * 20 + 1 : i * 20 + 21], rotation=45)
-    print("plotting")
-    plt.gcf().set_size_inches(10, 25)
-    plt.savefig(f"figs/{args.exp_name}_{args.exp2_name}_{idx}.png")
+    if args.type == "compare":
+        compare(args.exp_name, args.exp2_name, args.samples, args.idx, args.verbose)
+    elif args.type == "distrib":
+        plot_distribution(args.exp_name, args.exp2_name, args.idx, args.verbose)
+    elif args.type == "con":
+        plot_convergence(args.exp_name, args.exp2_name, args.idx, args.verbose)
 
 
 if __name__ == "__main__":
